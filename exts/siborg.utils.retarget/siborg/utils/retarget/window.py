147af9f776444d9f0b941e9da392e9d017db0ffe
Joshua Grebler <jhg29@njit.edu>
2023-08-11 17:42:43 -0400

    fix startup/shutdown

--- exts/siborg.utils.retarget/siborg/utils/retarget/window.py
+++ exts/siborg.utils.retarget/siborg/utils/retarget/window.py
@@ -10,13 +10,21 @@ class AnimationPreviewWindow(ui.Window):
 
         self.usd_context = omni.usd.create_context("anim_preview")
 
+        self.frame.set_build_fn(self._build_ui)
+
+
+    def _build_ui(self):
         with self.frame:
             self.__preview_widget = AnimationPreviewWidget(self.usd_context.get_name(), resolution=(600, 600))
+
 
     def __del__(self):
         self.destroy()
 
     def destroy(self):
-        self.__preview_widget.destroy()
-        self.__preview_widget = None
+        if self.__preview_widget:
+            self.__preview_widget.destroy()
+            self.__preview_widget = None
+        if self.usd_context:
+            self.usd_context.remove_all_hydra_engines()
         super().destroy()
